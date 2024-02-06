096e8a35eb785b0e510343c7e9c21efc72bbda52
arthurrrs <143718272+arthurrrs@users.noreply.github.com>
2024-02-06 11:39:37 -0300

    update atividades

--- zoológico.ts/index.ts
+++ zoológico.ts/index.ts
@@ -1,15 +1,17 @@
 import { Mamifero } from "./Mamifero";
 import { Ave } from "./Ave";
 
-const meuMamifero = new Mamifero("leao", 20, "branco")
-const papagaio = new Ave("Loro", 2, 1.2);
+    const meuMamifero = new Mamifero("leao", 20, "branco");
+    const papagaio = new Ave("loro", 2, "azul");
 
-console.log(`Nome do Papagaio: ${papagaio.nome}`);
-console.log(`Idade do Papagaio: ${papagaio.idade}`);
-console.log(`Envergadura do Papagaio: ${papagaio.envergadura}`);
+    console.log(`Nome do Papagaio: ${papagaio.nome}`);
+    console.log(`Idade do Papagaio: ${papagaio.idade}`);
+    console.log(`Envergadura do Papagaio: ${papagaio.envergadura}`);
+    loro.emitSOund();
+    loro.breastfeed();
 
-
-console.log(`Nome do Mamifero: ${Mamifero.nome}`);
-console.log(`Idade do Mamifero: ${Mamifero.idade}`);
-console.log(`Envergadura do Mamifero: ${Mamifero.envergadura}`);
-console.log(`Som do Mamifero: ${Mamifero.emitSound()}`);
+    console.log(`Nome do Mamifero: ${leao.nome}`);
+    console.log(`Idade do Mamifero: ${leao.idade}`);
+    console.log(`Envergadura do Mamifero: ${leao.pelagem}`);
+    leao.emitSound();
+    leao.breastfeed();
